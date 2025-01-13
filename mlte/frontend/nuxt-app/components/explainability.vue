43edb97d2eeb703955ddece535a5978ab33791f5
Belen Saavedra <saavedrariosm@berea.edu>
2025-01-13 16:47:26 -0500

    finished q2,q3 of explanability req

--- mlte/frontend/nuxt-app/components/explainability.vue
+++ mlte/frontend/nuxt-app/components/explainability.vue
@@ -9,23 +9,23 @@
   </div>
   <br />
 
-<!-- Table-->
-          <div v-for="(field, fieldIndex) in dataItem.fields" :key="fieldIndex">
-            <h3 class="no-margin-sub-header">
-             <b>Stakeholder or Recipient {{ fieldIndex + 1 }} </b> 
-             <InfoIcon>
-                      Field type, e.g., number, string, Boolean, data, image,
-                      audio. 
-                      <br/>
-                    </InfoIcon>
-            </h3>
+<!-- List of stakeholders-->
+  <div v-for="(field, fieldIndex) in dataItem.fields" :key="fieldIndex">
+     <h3 class="no-margin-sub-header">
+      <b>Stakeholder or Recipient {{ fieldIndex + 1 }} </b> 
 
-            <!-- 1) Stakeholders List -->
+         <div class="info-container">
+            <span class="info-icon">i</span>
+            <div class="tooltip"> Select the person or party for whom you are writing the requirement..</div>
+           </div>
+     </h3>
+  <!-- Q1) Select the generated Stakeholders List -->
   <USelect
     placeholder="Select an option..."
     icon="i-heroicons-magnifying-glass-20-solid"
     v-model="stakeholder"
     :options="stakeholder_list"
+    @change="onStakeholderChange" 
   />
   <!-- Conditionally show input field for 'Other' -->
   <br />
@@ -35,41 +35,72 @@
     <br />
   </div>
 
-  <!-- 2) Purpose -->
-  <label><b>Purpose of the Explanations</b></label>
-  <InfoIcon>
-                      Field type, e.g., number, string, Boolean, data, image,
-                      audio.
-                    </InfoIcon>
-  <USelect
-    placeholder="Select an option..."
+  <!-- Q2)generate a list of purposes based on the stakeholder previously selected -->
+  <label><b>Purposes for Explainability</b></label>
+
+
+    <div class="info-container">
+      <span class="info-icon">i</span>
+      <div class="tooltip"> Select the purposes for explainability related to the previously selected stakeholder.</div>
+    </div>
+  <USelectMenu
+    multiple placeholder="Select an option..."
     icon="i-heroicons-magnifying-glass-20-solid"
     v-model="purpose"
     :options="purpose_list"
   />
-
   <br/>
-  <label><b>Explainability Expectations</b></label>
+
+  <label><b>Type of Explanation</b></label>
+
+   <div class="info-container">
+      <span class="info-icon">i</span>
+      <div class="tooltip">{{ Q3_response }}</div>
+    </div>
+
+   <USelectMenu
+    multiple placeholder="Select an option..."
+    icon="i-heroicons-magnifying-glass-20-solid"
+    v-model="typeofExplanation"
+    :options="['Global Explanation', 'Local Explanation', 'Data Disclosure', 'Other']"
+  />
+  <br/>
+
+
+
+  <label><b>Language Level</b></label>
+  <div class="info-container">
+  <span class="info-icon">e</span>
+  <div class="tooltip">{{ Q4_response }}</div>
+  </div>
 
   <UInput v-model="language_expectations" />
 
   <br/>
   <label><b>Test Plan</b></label>
+  <span class="info-icon">e</span>
+            <div class="tooltip"> Select the person or party for whom you are writing the requirement..</div>
+          
 
   <UInput v-model="test_plan" />
+  <br/>
 
-                 <DeleteButton
-              class="margin-button"
-              @click="deleteField(dataItemIndex, fieldIndex)"
-            >
-              Delete Stakeholder
-            </DeleteButton>
-            <hr />
-          </div>
+   <!-- Dynamic Sentence -->
+   <p class="input-group" style="padding-top: 10px; padding-bottom: 10px">
+  <b>Scenario for Training Latency:</b> The model's retraining latency is expected to be [{{ ExpectedLatency }}] {{unit_averagelatency}} using [{{ retrainingMethod }}]. The source of the retraining data is from [{{ DataSource }}].
+</p>
+<br/>
 
-          <AddButton class="margin-button" @click="addField(dataItemIndex)">
-            Add Stakeholder
-          </AddButton>
+
+      <DeleteButton class="margin-button" @click="deleteField(dataItemIndex, fieldIndex)">
+         Delete Requirement
+      </DeleteButton>
+        <hr />
+      </div>
+
+      <AddButton class="margin-button" @click="addField(dataItemIndex)">
+            Add Requirement
+      </AddButton>
         
 
 
@@ -88,10 +119,13 @@ export default {
   },
   setup(props) {
     const response = ref('');
+    const Q3_response = ref('');
+    const Q4_response = ref('');
     const stakeholder = ref('');
     const stakeholder_list = ref([]);
     const purpose = ref([]);
     const purpose_list = ref([]);
+    const typeofExplanation = ref([]);
     const OtherStakeholder = ref('');
     const showOtherInput = ref(false);
 
@@ -156,7 +190,7 @@ export default {
           },
           {
             role: 'user',
-            content: `Write a concise sentence explaining a potential consequence of neglecting proper explainability in the context of ${props.MLTask} and ${props.usageContext} for a non-technical stakeholder of the product.`,
+            content: `Write a concise sentence explaining a potential consequence of neglecting proper explainability in the context of ${props.MLTask} and ${props.usageContext} for a non-technical stakeholder of the product. Instead of mentioning trust as a requirement, provide an example of what that means.`,
           },
         ];
         const chatResponse = await chat(messages, 'gpt-3.5-turbo');
@@ -195,8 +229,8 @@ export default {
         console.error('Error fetching chat response for stakeholders:', error);
       }
     };
-
-    const GetPurpose = async () => {
+  // Fetch purposes dynamically based on stakeholder
+  const GetPurpose = async (selectedStakeholder) => {
       const { chat } = openai();
       try {
         const messages = [
@@ -206,14 +240,15 @@ export default {
           },
           {
             role: 'user',
-            content: `Generate purposes for explainability in the context of ${props.MLTask} and ${props.usageContext}.`,
+            content: `Generate purposes for explainability for a ${selectedStakeholder.value} in the context of ${props.MLTask} and ${props.usageContext}. Provide an unnumbered list. Do not mention trust without explaining what that means in this context.
+            Provide brief sentences of around 15 words.  `,
           },
         ];
 
         const chatPurpose = await chat(messages, 'gpt-3.5-turbo');
         const PurposeArray = chatPurpose
           .split('\n')
-          .filter(purpose => purpose.trim() !== '')
+          .filter((purpose) => purpose.trim() !== '')
           .map((purpose, index) => ({
             label: purpose.replace(/^-/, '').trim(),
             value: `purpose_${index}`,
@@ -226,25 +261,119 @@ export default {
       }
     };
 
+
+    // watching when the stakeholder has been selected:
+    // Watch for stakeholder selection changes
+    const onStakeholderChange = (newStakeholder) => {
+      if (newStakeholder === 'stakeholder_other') {
+        showOtherInput.value = true;
+      } else {
+        showOtherInput.value = false;
+        GetPurpose(newStakeholder); 
+        Q3(newStakeholder);
+      }
+    };
+
+    // Info icon on Q3: type of explanation 
+    const Q3 = async (selectedStakeholder) => {
+      const { chat } = openai();
+      try {
+        const messages = [
+          {
+            role: 'system',
+            content: 'You are a product manager with experience in managing AI products.',
+          },
+          {
+            role: 'user',
+            content: `
+            
+            Provide a concise suggestion of 15 words max on the type of explanation a data scientist should consider for a model targeted for 
+            ${selectedStakeholder.value} would require for the following ML model: ${props.MLTask}, used in the context of ${props.usageContext}. 
+
+        The explanation types options are: 
+          - Global explainations: High-level understanding of the model's overall behavior.
+          - Local explanations: Insights into specific predictions or decisions.
+          - Data Disclosure: Transparency regarding the data used to train or inform the model.
+          - Other
+
+        Briefly justify your suggestion by explaining why this explanation type is most relevant or valuable for this specific stakeholder in the given context. 
+        You can suggest more than one option.
+
+     
+`,
+          },
+        ];
+
+        const chatResponse = await chat(messages, 'gpt-3.5-turbo');
+        Q3_response.value = chatResponse.split('\n\n')[0];
+        
+      } catch (error) {
+        console.error('Error fetching chat response for stakeholders:', error);
+      }
+    };
+
+    // Example Info icon: language level question. 
+
+    const Q4 = async (selectedStakeholder) => {
+      const { chat } = openai();
+      try {
+        const messages = [
+          {
+            role: 'system',
+            content: 'You are a product manager with experience in managing AI products.',
+          },
+          {
+            role: 'user',
+            content: `
+            
+            Provide an example of the language level requirement that a data scientist should consider when meeting 
+            the explainability expectations for ${selectedStakeholder.value} who has the following purposes for exaplainability: ${purpose} and 
+            the type of explanations for a ${props.MLTask} is ${typeofExplanation}.
+     
+`,
+          },
+        ];
+
+        const chatResponse = await chat(messages, 'gpt-3.5-turbo');
+        Q4_response.value = chatResponse.split('\n\n')[0];
+        
+      } catch (error) {
+        console.error('Error fetching chat response for stakeholders:', error);
+      }
+    };
+
+    watch([stakeholder, purpose, typeofExplanation], ([newStakeholder, newPurpose, newTypeofExplanation]) => {
+    if (newStakeholder && newPurpose.length > 0 && newTypeofExplanation.length > 0) {
+      Q4();
+    }
+  });
+
+
     onMounted(() => {
       getChatResponse();
       GetStakeholders();
       GetPurpose();
+      Q3();
+      Q4();
     });
 
     return {
       response,
+      Q3_response,
+      Q4_response,
       stakeholder,
       stakeholder_list,
       purpose,
       purpose_list,
       showOtherInput,
+      typeofExplanation,
       OtherStakeholder,
       dataModalHeaders,
       dataModalRows,
       dataItem,
       addField,
       deleteField,
+      onStakeholderChange,
     };
   },
 };
@@ -254,5 +383,47 @@ export default {
 <style scoped>
 .AIgeneratedtext {
   background-color: #efe8c7;
+}
+
+.info-icon {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-color: black;
+  color: white;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 20px;
+  font-weight: bold;
+  font-family: Arial, cursive;
+  font-size: 10px;
+  cursor: pointer;
+  margin-left: 5px;
+  position: relative;
+}
+
+.tooltip {
+  display: none;
+  position: absolute;
+  background-color: rgb(0, 0, 0);
+  color: rgb(255, 255, 255);
+  border: 1px solid #ccc;
+  padding: 10px;
+  font-size: 12px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 200px;
+  top: 25px; 
+  left: 0;
+  z-index: 10;
+}
+
+.info-container:hover .tooltip {
+  display: block;
+}
+
+.info-container {
+  position: relative;
+  display: inline-block;
 }
 </style>
